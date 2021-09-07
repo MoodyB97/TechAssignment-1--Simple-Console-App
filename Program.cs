2a26f8dbbe720101e609f453c3f8bfdde844d822
Benjamin Moody <moodyb@usf.edu>
2021-09-07 16:02:09 -0400

    Hello WOrld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechAssignment_1__Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy there Partner!!!");
+        }
+    }
+}
